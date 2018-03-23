1e5bcf589e00740c8c92d82a68d7fbfa718c1bed
Mohamed Gaber <m.gaber.ali92@gmail.com>
2018-03-23 21:00:50 +0200

    Configure Flyway

---
+++ src/main/java/com/mg/flashcards/config/FlywayConfig.java
@@ -0,0 +1,24 @@
+package com.mg.flashcards.config;
+
+import org.flywaydb.core.Flyway;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class FlywayConfig {
+
+    @Bean
+    @Profile("dev")
+    public Flyway flyway(DataSource dataSource){
+        Flyway flyway = new Flyway();
+        flyway.setDataSource(dataSource);
+        flyway.setLocations("classpath:db/migration");
+        flyway.clean();
+        flyway.migrate();
+        return flyway;
+    }
+
+}
