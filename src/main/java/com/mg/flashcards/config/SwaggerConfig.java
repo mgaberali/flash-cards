6f6bd50fc248015ef5768b6079d59195584e25c1
Mohamed Gaber <m.gaber.ali92@gmail.com>
2018-07-20 15:55:47 +0200

    Config swagger to work with oauth2

--- src/main/java/com/mg/flashcards/config/SwaggerConfig.java
+++ src/main/java/com/mg/flashcards/config/SwaggerConfig.java
@@ -3,14 +3,19 @@ package com.mg.flashcards.config;
 import com.mg.flashcards.rest.controllers.RestControllersMarker;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.AuthorizationCodeGrantBuilder;
+import springfox.documentation.builders.OAuthBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.web.SecurityConfiguration;
+import springfox.documentation.swagger.web.SecurityConfigurationBuilder;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.util.Arrays;
 import java.util.Collections;
 
 @Configuration
@@ -24,7 +29,9 @@ public class SwaggerConfig {
                 .apis(RequestHandlerSelectors.basePackage(RestControllersMarker.class.getPackage().getName()))
                 .paths(PathSelectors.any())
                 .build()
-                .apiInfo(apiInfo());
+                .apiInfo(apiInfo())
+                .securitySchemes(Arrays.asList(securityScheme()))
+                .securityContexts(Arrays.asList(securityContext()));
     }
 
     private ApiInfo apiInfo() {
@@ -38,4 +45,38 @@ public class SwaggerConfig {
                 Collections.emptyList());
     }
 
+    @Bean
+    public SecurityConfiguration security() {
+        return SecurityConfigurationBuilder.builder()
+                .clientId("web")
+                .clientSecret("123456")
+                .scopeSeparator(" ")
+                .useBasicAuthenticationWithAccessCodeGrant(true)
+                .build();
+    }
+
+    private SecurityScheme securityScheme() {
+        GrantType grantType = new ResourceOwnerPasswordCredentialsGrant("http://localhost:8080/oauth" + "/token");
+
+        SecurityScheme oauth = new OAuthBuilder().name("spring_oauth")
+                .grantTypes(Arrays.asList(grantType))
+                .scopes(Arrays.asList(scopes()))
+                .build();
+        return oauth;
+    }
+
+    private AuthorizationScope[] scopes() {
+        AuthorizationScope[] scopes = {
+                new AuthorizationScope("read", "for read operations"),
+                new AuthorizationScope("write", "for write operations")};
+        return scopes;
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder()
+                .securityReferences(
+                        Arrays.asList(new SecurityReference("spring_oauth", scopes())))
+                .forPaths(PathSelectors.regex("/api.*"))
+                .build();
+    }
 }
